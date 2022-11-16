5e265d8db9af18daa86104ecc0bf271c2f7c00ee
hitosiki <114006619+hitosiki@users.noreply.github.com>
2022-11-16 14:31:57 +0100

    Initial Commit

---
+++ Apple Store/Apple_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_StoreApp.swift
+//  Apple Store
+//
+//  Created by Lorenzo Amabile on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Apple_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
